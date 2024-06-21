76215505ff79a8a47a0652ed27ff8c20b41a824c
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-21 17:04:14 +0530

    chore: commit to rebuild after changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div className="font-space-grotesk text-2xl">App</div>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center font-space-grotesk text-2xl">
+      App
+    </div>
+  );
 };
 
 export default App;
