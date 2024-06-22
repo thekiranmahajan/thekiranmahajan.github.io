28d654d4d2af6a76107c97383edf8939db61198b
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-22 20:31:42 +0530

    feat: fully functional bottom navbar for smooth navigation on small screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { IoGridOutline, IoMoonOutline, IoSunnyOutline } from "react-icons/io5";
+import { IoGridOutline, IoMoonOutline } from "react-icons/io5";
 import NavLinks from "./NavLinks";
 import { HiMiniXMark } from "react-icons/hi2";
 const Header = () => {
@@ -9,7 +9,7 @@ const Header = () => {
       <h2 className="font-rajdhani cursor-pointer text-lg font-bold">
         Kiran Mahajan<span className="text-yellow dark:text-violet">.</span>
       </h2>
-      <div className="flex h-full w-1/3 items-center justify-around">
+      <div className="flex h-full w-16 items-center justify-between">
         {/* <IoSunnyOutline className="text-xl" /> */}
         <IoMoonOutline className="text-xl" />
         <IoGridOutline
@@ -17,11 +17,17 @@ const Header = () => {
           className="focus:bg-violet text-xl"
         />
       </div>
+
       <div
-        className={`dark:bg-yellow bg-light-blue dark:text-dark-blue absolute bottom-0 left-1/2 z-10 flex h-56 w-full -translate-x-1/2 flex-col p-1 text-white ${isMenuOpen ? "flex translate-y-0" : "hidden translate-y-96"}`}
+        className={`dark:bg-yellow bg-light-blue dark:text-dark-blue absolute bottom-0 left-1/2 right-0 z-10 flex h-56 w-full -translate-x-1/2 transform flex-col rounded-t-2xl p-1 text-white shadow-md transition-transform duration-200 ${isMenuOpen ? "flex translate-y-0" : "translate-y-full"} `}
       >
         <NavLinks />
-        <HiMiniXMark className="ml-auto text-2xl" />
+        <div className="flex h-12 w-full items-center justify-end px-3">
+          <HiMiniXMark
+            onClick={() => setIsMenuOpen(false)}
+            className="text-violet text-2xl"
+          />
+        </div>
       </div>
     </header>
   );
