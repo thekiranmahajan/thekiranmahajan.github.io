8059d594dee7ae0a81cc0ff247779a18c8db3390
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-22 18:03:25 +0530

    style: configure custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ export default {
         "space-grotesk": ['"Space Grotesk"', ...defaultTheme.fontFamily.sans],
         rajdhani: ['"Rajdhani"', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        yellow: "#FFBF18",
+        white: "#FFFFFF",
+        violet: "#6F01Ec",
+        "dark-blue": "#100E34",
+        "light-blue": "#4F48EC",
+      },
     },
   },
   plugins: [],
