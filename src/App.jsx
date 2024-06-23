6e299e3ccd384b966c41cf2351f8a44f8daccf76
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-23 13:47:13 +0530

    style: bg color transition smooth

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Header } from "./components";
 
 const App = () => {
   return (
-    <main className="relative flex min-h-screen w-screen flex-col overflow-x-hidden bg-white font-space-grotesk dark:bg-[#100E34]">
+    <main className="relative flex min-h-screen w-screen flex-col overflow-x-hidden bg-white font-space-grotesk transition-colors duration-500 dark:bg-[#100E34]">
       <Header />
     </main>
   );
