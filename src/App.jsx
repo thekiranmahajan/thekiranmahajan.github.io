02c862282fc9ce41d6a3c341a921df03fd216187
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-23 20:55:55 +0530

    feat: add name and social icons to footer + improve responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Footer, Header } from "./components";
-import { Home } from "./sections";
+import { About, Contact, Experience, Home, Projects, Skills } from "./sections";
 
 const App = () => {
   return (
     <main className="relative flex min-h-screen w-full flex-col overflow-x-hidden bg-white font-space-grotesk transition-colors duration-500 dark:bg-[#100E34]">
       <Header />
       <Home />
+      <About />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Contact />
       <Footer />
     </main>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,29 @@
+import { FaXTwitter } from "react-icons/fa6";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
+import { MdOutlineEmail } from "react-icons/md";
+
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className="mt-auto flex h-64 w-full flex-col items-center bg-black text-white">
-      <div>
-        
+    <footer className="mt-auto flex h-80 w-full flex-col items-center bg-black px-5 pt-5 text-white md:h-60 md:pt-0">
+      <div className="flex w-full justify-between gap-4 md:mt-6 lg:w-7/12">
+        <div className="mt-10 md:mt-4">
+          <h1 className="font-rajdhani text-3xl font-extrabold md:text-4xl">
+            Kiran Mahajan
+            <span className="text-yellow dark:text-violet">.</span>
+          </h1>
+          <h3 className="text-sm text-gray-200 md:text-base">
+            Software Development Engineer
+          </h3>
+        </div>
+        <div className="flex h-full flex-col items-center justify-center gap-6 text-lg sm:text-xl md:flex-row md:text-2xl">
+          <FiLinkedin />
+          <FiGithub />
+          <MdOutlineEmail />
+          <FaXTwitter />
+        </div>
       </div>
-      <div className="mb-20 mt-auto flex w-full items-center justify-center text-sm text-gray-400">
+      <div className="mb-20 mt-auto flex w-full items-center justify-center text-xs text-gray-400 sm:text-sm md:mb-10">
         &copy; Kiran Mahajan. All Rights Reserved {year}
       </div>
     </footer>
