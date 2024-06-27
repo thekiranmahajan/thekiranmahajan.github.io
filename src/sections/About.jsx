06edc3f22467c96612f9dd19a84dbbfadc194918
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-27 21:01:05 +0530

    style: responsiveness fixes

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -9,15 +9,15 @@ const About = () => {
     >
       <SectionHeading headingText="About" />
       <SectionSubHeading subHeadingText="Meet Kiran" />
-      <div className="flex h-full w-full flex-col items-center justify-between gap-6 px-3 pt-10 sm:flex-row sm:items-start sm:justify-evenly sm:gap-0 sm:pt-16">
-        <div className="flex items-center justify-end sm:w-2/5">
+      <div className="flex h-full w-full flex-col items-center justify-between gap-6 px-3 pt-10 sm:w-11/12 sm:flex-row sm:items-start sm:justify-evenly sm:gap-10 sm:pb-10 sm:pt-16 md:w-4/5 md:gap-14">
+        <div className="flex items-center justify-end sm:w-1/2">
           <img
-            className="h-52 w-52 overflow-hidden rounded-xl object-cover object-center shadow-md md:h-64 md:w-64"
+            className="h-52 w-52 overflow-hidden rounded-xl object-cover object-center shadow-md md:h-64 md:w-64 lg:h-72 lg:w-72"
             src={profilePic00}
             alt="Kiran Mahajan's Profile Picture"
           />
         </div>
-        <p className="w-full pb-2 text-center text-gray-900 sm:w-2/5 sm:text-left dark:text-gray-200">
+        <p className="w-full pb-2 text-center text-gray-900 sm:w-1/2 sm:text-left dark:text-gray-200">
           Konnichiwa🌸, <br /> Kiran here (
           <a href="https://en.wikipedia.org/wiki/Kirron_Kher" target="_blank">
             !Kher
