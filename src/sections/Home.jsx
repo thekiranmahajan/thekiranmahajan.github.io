e270af067762cce12c5beb72e5190030bf48872b
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-27 10:26:24 +0530

    feat: add animation to rocket.png using framer motion

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -2,6 +2,7 @@ import { FaArrowRight } from "react-icons/fa6";
 import rocket from "../assets/rocket.png";
 import { EncryptTextEffectV2, FlipTextEffect } from "../components";
 import { LuMouse } from "react-icons/lu";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
@@ -9,14 +10,7 @@ const Home = () => {
       id="home"
       className="relative flex min-h-screen w-full flex-col items-center gap-5 p-10 px-4 md:px-5 md:pt-44 lg:w-4/5"
     >
-      <div className="flex h-full w-full flex-col items-center md:flex-row md:items-start md:justify-around">
-        <div className="mb-16 h-36 w-36 md:mb-0 md:mt-5 md:h-44 md:w-44">
-          <img
-            className="h-full w-full select-none drop-shadow-2xl"
-            src={rocket}
-            alt="rocket"
-          />
-        </div>
+      <div className="flex h-full w-full flex-col-reverse items-center md:flex-row md:items-start md:justify-around">
         <div className="w-fit overflow-hidden dark:text-white">
           <h3 className="text-lg font-semibold">Hello,👋🏽 I&apos;m</h3>
           <FlipTextEffect>Kiran</FlipTextEffect>
@@ -32,6 +26,26 @@ const Home = () => {
             Resume <FaArrowRight />
           </button>
         </div>
+        <motion.div
+          initial={{
+            x: 30,
+            y: 30,
+          }}
+          animate={{ x: -30, y: -30 }}
+          transition={{
+            repeat: Infinity,
+            repeatType: "reverse",
+            duration: 3,
+            ease: "easeInOut",
+          }}
+          className="mb-16 h-36 w-36 md:mb-0 md:mt-5 md:h-44 md:w-44"
+        >
+          <img
+            className="h-full w-full select-none drop-shadow-2xl"
+            src={rocket}
+            alt="rocket"
+          />
+        </motion.div>
       </div>
       <h2 className="mt-16 flex items-center gap-2 text-center md:mt-32">
         <LuMouse />
